7a5534967d90e8d9d8011d68215a007d7588a53f
tremesh <112059618+tremesh@users.noreply.github.com>
2022-08-25 13:03:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title (" My First Streamlit App")
 streamlit.header('Break Fast Menu')
-print ("\U0001f600")
+print("\U0001F601")
+print("\U0001F605")
 streamlit.text ('3 Idli with Chutney and Sambar')
 streamlit.text('A glass of juice')
