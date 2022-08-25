796948f6871af69672817d937da13d99da68f43c
tremesh <112059618+tremesh@users.noreply.github.com>
2022-08-25 13:35:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
+import pandas
 streamlit.title (" My First Streamlit App")
+mylist = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(mylist)
 
