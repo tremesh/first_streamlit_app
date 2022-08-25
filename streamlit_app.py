59466845df6b81eac6cad9999314fec684f10131
tremesh <112059618+tremesh@users.noreply.github.com>
2022-08-25 17:35:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas
 streamlit.title (" My First Streamlit App")
 streamlit.header ("My Veg Basket")
 mylist = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+mylist = mylist.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(mylist.index),['Avocado','Strawberries'])
 fruits_to_show = mylist.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
